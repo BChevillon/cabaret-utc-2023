604492d0e8dc5c7076d6a67026be290a2b18f95c
Benoit CHEVILLON <benoit.chevillon6@gmail.com>
2023-11-13 16:42:09 +0100

    Fix waiters form link

--- src/components/Waiter.js
+++ src/components/Waiter.js
@@ -11,7 +11,7 @@ export default function Waiter() {
       <p style={{fontSize: "1.35em"}}>Tu veux vivre le cabaret differement ?</p>
       <p style={{marginBottom: "1em", fontSize: "1.35em"}}>Tu as raté le shotgun et tu veux quand même profiter de la soirée ? Tu peux devenir serveur !</p>
       <div style={{margin: "0 auto", width: "250px"}}>
-        <a href="" style={{textDecoration: "none"}}>
+        <a href="https://forms.gle/x31y6v2kKQKr9ETJ7" style={{textDecoration: "none"}}>
           <button class='btn-slack'>
             Devenir serveur
             <img src={glass} alt="Logo serveur" />
